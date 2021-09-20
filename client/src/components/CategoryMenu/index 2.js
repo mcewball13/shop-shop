95bfed264cdfaeb314d231a6b5ad6d83817ad4a4
mcewball13 <mcewen1984@hotmail.com>
2021-09-20 15:02:46 -0700

    finished shopping cart functionality

---
+++ client/src/components/CategoryMenu/index 2.js
@@ -0,0 +1,46 @@
+import React, {useEffect} from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES} from '../../utils/queries';
+import { UPDATE_CURRENT_CATEGORY, UPDATE_CATEGORIES} from "../../utils/actions"
+import { useStoreContext } from "../../utils/GlobalState";
+
+
+function CategoryMenu() {
+const [state, dispatch] = useStoreContext();
+const {categories} = state;
+const {data: categoryData} = useQuery(QUERY_CATEGORIES)
+
+useEffect(() => {
+  if (categoryData) {
+    dispatch({
+      type: UPDATE_CATEGORIES,
+      categories: categoryData.categories
+    })
+  }
+}, [categoryData, dispatch]);
+
+const handleClick = id => {
+  dispatch({
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: id
+  })
+}
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,56 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
-import { QUERY_PRODUCTS } from '../utils/queries';
-import spinner from '../assets/spinner.gif';
+import {
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    ADD_TO_CART,
+    UPDATE_PRODUCTS,
+} from "../utils/actions";
+import { QUERY_PRODUCTS } from "../utils/queries";
+import spinner from "../assets/spinner.gif";
+import Cart from "../components/Cart";
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
-const { id } = useParams();
+    const [state, dispatch] = useStoreContext();
+    const { products, cart } = state;
 
-const [currentProduct, setCurrentProduct] = useState({})
+    const { id } = useParams();
 
-const { loading, data } = useQuery(QUERY_PRODUCTS);
+    const [currentProduct, setCurrentProduct] = useState({});
 
-const { products } = state;
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-useEffect(() => {
-  if (products.length) {
-    setCurrentProduct(products.find(product => product._id === id));
-  } else if (data) {
-    dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products
-    });
-  }
-}, [products, data, dispatch, id]);
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id);
 
-          <h2>{currentProduct.name}</h2>
+        if (itemInCart) {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                product: { ...currentProduct, purchaseQuantity: 1 },
+            });
+        }
+    };
 
-          <p>{currentProduct.description}</p>
+    useEffect(() => {
+        if (products.length) {
+            setCurrentProduct(products.find((product) => product._id === id));
+        } else if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products,
+            });
+        }
+    }, [products, data, dispatch, id]);
 
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
+    const removeFromCart = () => {
+        dispatch({
+            type: REMOVE_FROM_CART,
+            _id: currentProduct._id,
+        });
+    };
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
+    return (
+        <>
+            {currentProduct ? (
+                <div className="container my-1">
+                    <Link to="/">← Back to Products</Link>
+
+                    <h2>{currentProduct.name}</h2>
+
+                    <p>{currentProduct.description}</p>
+
+                    <p>
+                        <strong>Price:</strong>${currentProduct.price}{" "}
+                        <button onClick={addToCart}>Add to Cart</button>
+                        <button
+                            disabled={
+                                !cart.find((p) => p._id === currentProduct._id)
+                            }
+                            onClick={removeFromCart}
+                        >
+                            Remove from Cart
+                        </button>
+                    </p>
+
+                    <img
+                        src={`/images/${currentProduct.image}`}
+                        alt={currentProduct.name}
+                    />
+                </div>
+            ) : null}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+            <Cart />
+        </>
+    );
 }
 
 export default Detail;
