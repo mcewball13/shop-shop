6577225756f3f6c8a91aad63804f5ec7ce56ca2f
mcewball13 <mcewen1984@hotmail.com>
2021-09-27 16:59:16 -0700

    deploying to heroku

---
+++ client/src/components/CategoryMenu/index 2.js
@@ -0,0 +1,59 @@
+import React, {useEffect} from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES} from '../../utils/queries';
+import { UPDATE_CURRENT_CATEGORY, UPDATE_CATEGORIES} from "../../utils/actions"
+import { useDispatch, useSelector } from 'react-redux';
+import { idbPromise } from '../../utils/helpers';
+
+
+function CategoryMenu() {
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+
+const {categories} = state;
+const {loading, data: categoryData} = useQuery(QUERY_CATEGORIES)
+
+useEffect(() => {
+  if (categoryData) {
+    dispatch({
+      type: UPDATE_CATEGORIES,
+      categories: categoryData.categories
+    });
+    categoryData.categories.forEach(category => {
+      idbPromise('categories', 'put', category);
+    });
+  } else if (!loading) {
+    idbPromise('categories', 'get').then(categories => {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categories
+      });
+    });
+  }
+}, [categoryData, loading, dispatch]);
+
+const handleClick = id => {
+  dispatch({
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: id
+  })
+}
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
